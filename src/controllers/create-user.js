f914dc650a829f1a4a8a5502da913d747e90c545
Ivan-Leonardi <ivanthaisjesus@gmail.com>
2024-07-21 17:34:13 -0300

    feat: add index.js file to use cases and repositories

--- src/controllers/create-user.js
+++ src/controllers/create-user.js
@@ -1,4 +1,4 @@
-import { CreateUserUseCase } from '../use-cases/create-user.js';
+import { CreateUserUseCase } from '../use-cases/index.js';
 import { EmailAlreadyInUseError } from '../errors/user.js';
 import { checkIfEmailIsValid, checkIfPasswordIsValid, emailIsAlreadyInUseResponse, invalidPasswordResponse, badRequest, created, serverError } from './helpers/index.js';
 export class CreateUserController {
